917dcc90a96e3c89c33d6665bfaeee1cc6844ac3
Tim <harvard90873@gmail.com>
2022-08-21 23:21:14 +0200

    init web

---
+++ web/pages/api/predict.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
+
+  const {} = req.body;
+}
