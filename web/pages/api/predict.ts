51072b44a17ed89947a03e00680bf5cc3fa06af8
Tim <harvard90873@gmail.com>
2022-08-22 00:31:16 +0200

    js format model

--- web/pages/api/predict.ts
+++ web/pages/api/predict.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import {} from "@tensorflow/tfjs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
